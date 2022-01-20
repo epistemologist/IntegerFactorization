feba6c6367e22f5aaa54f2e2037d31bc898bba31
Husnain Raza <husnain2@illinois.edu>
2022-01-20 12:03:23 +0500

    Trying again....

--- factorizer_qs.py
+++ factorizer_qs.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 from itertools import count
 from typing import Optional, List
 from math import prod, gcd
+from random import sample
+
 from factorizer_abstract import Factorizer, FactorList
 from utility import get_primes, legendre, tonelli_shanks, isqrt
 
@@ -75,62 +77,67 @@ class QuadraticSieve(Factorizer):
         for chunk_start in count(isqrt(N)+1, BLOCK_LENGTH):
             candidates.update(_process_chunk(chunk_start))
             print(len(candidates))
-            if len(candidates) >= len(primes) + 1:
+            if len(candidates) >= 2*len(primes):
                 break
 
-        # Linear algebra
-        candidates = OrderedDict([(c, candidates[c]) for c in list(sorted(candidates))[:len(primes)+1]])
-        exponent_vectors = []
-        for c in candidates:
-            factorization = candidates[c].to_dict()
-            exponent_vectors.append([factorization[p] % 2 for p in primes ])
-	# Inspired by https://github.com/mikolajsawicki/quadratic-sieve/blob/main/quadratic_sieve/fast_gauss.py
-        # Also referred to https://www.cs.umd.edu/~gasarch/TOPICS/factoring/fastgauss.pdf
-        M = np.vstack(exponent_vectors)
-        print(f"M: {M}")
-        def _to_rref(M):
-            n,m = M.shape
-            marked = []
-            for j in range(m):
-                print(j)
-                i = 0
-                while i < n and M[i,j] != 1:
-                    i += 1
-                if i == n:
-                    continue
-                else:
-                    marked.append(i)
-                    for k in range(m):
-                        if k != j:
-                            if M[i, k] == 1:
-                                M[:, k] ^= M[:, j]
-            return marked
-        marked = _to_rref(M)
-        print(f"marked: {marked}")
-        dependent_index = [i for i in range(len(M)) if i not in marked][0]
-        print(f"dependent index: {dependent_index}")
-        basis = [dependent_index]
-        ones_cols = np.where(M[dependent_index] == 1)[0]
-        print(f"ones_cols: {ones_cols}")
-        basis.extend([ np.where(M[:, j] == 1)[0][0] for j in ones_cols ])
-        # Factorization step
-        x_s = []
-        y_s = []
-        C = list(candidates.keys())
-        for b in basis:
-            x_s.append(C[b])
-            y_s.append(candidates[C[b]])
-        def _sqrt_prod( nums: List[FactorList] ) -> FactorList:
-            out = defaultdict(int)
-            for n in nums:
-                for p in n:
-                    out[p] += n[p]
-            if any([i % 2 != 0 for i in out.values()]): raise ValueError("not perfect square!")
-            return FactorList({ p: (out[p]) // 2 for p in out })
-        x = prod(x_s)
-        y = _sqrt_prod(y_s).prod()
-        d = gcd(x-y, N)
-        return d
+        # While we haven't found a factor...
+        while True:
+            # Linear algebra
+            candidates = OrderedDict([(c, candidates[c]) for c in sample(list(candidates), len(primes)+1)])
+            exponent_vectors = []
+            for c in candidates:
+                factorization = candidates[c].to_dict()
+                exponent_vectors.append([factorization[p] % 2 for p in primes ])
+            # Inspired by https://github.com/mikolajsawicki/quadratic-sieve/blob/main/quadratic_sieve/fast_gauss.py
+            # Also referred to https://www.cs.umd.edu/~gasarch/TOPICS/factoring/fastgauss.pdf
+            M = np.vstack(exponent_vectors)
+            print(f"M: {M}")
+            def _to_rref(M):
+                n,m = M.shape
+                marked = []
+                for j in range(m):
+                    print(j)
+                    i = 0
+                    while i < n and M[i,j] != 1:
+                        i += 1
+                    if i == n:
+                        continue
+                    else:
+                        marked.append(i)
+                        for k in range(m):
+                            if k != j:
+                                if M[i, k] == 1:
+                                    M[:, k] ^= M[:, j]
+                return marked
+            marked = _to_rref(M)
+            print(f"marked: {marked}")
+            dependent_index = [i for i in range(len(M)) if i not in marked][0]
+            print(f"dependent index: {dependent_index}")
+            basis = [dependent_index]
+            ones_cols = np.where(M[dependent_index] == 1)[0]
+            print(f"ones_cols: {ones_cols}")
+            basis.extend([ np.where(M[:, j] == 1)[0][0] for j in ones_cols ])
+            # Factorization step
+            x_s = []
+            y_s = []
+            C = list(candidates.keys())
+            for b in basis:
+                x_s.append(C[b])
+                y_s.append(candidates[C[b]])
+            def _sqrt_prod( nums: List[FactorList] ) -> FactorList:
+                out = defaultdict(int)
+                for n in nums:
+                    for p in n:
+                        out[p] += n[p]
+                if any([i % 2 != 0 for i in out.values()]): raise ValueError("not perfect square!")
+                return FactorList({ p: (out[p]) // 2 for p in out })
+            x = prod(x_s)
+            y = _sqrt_prod(y_s).prod()
+            d = gcd(x-y, N)
+            if d != 1:
+                return d
+            else:
+                print("d == 1, trying again...")
 
 N = 8754660968220887821
 # N = 1413409093
